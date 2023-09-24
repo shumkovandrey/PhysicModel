1734e754bc0c7916060eae49a6a66e77fd331bfb
andreyshumkov <shumkovandrey520@gmail.com>
2023-09-24 21:14:33 +0500

    0.2

--- main.py
+++ main.py
@@ -23,31 +23,32 @@ class Molecule(pg.sprite.Sprite):
         other_mols.remove(self)
         mol_hits = pg.sprite.spritecollide(self, other_mols, False)
         for m in mol_hits:
-            """if self.rect.right > m.rect.left:
-                self.rect.right = m.rect.left
-                self.velocity_x = 0
-            if self.rect.left < m.rect.right:
-                self.rect.left = m.rect.right
+            print(self.rect.right, m.rect.left)
+            if self.rect.right-1 - m.rect.left <= 5:
+                self.rect.x = m.rect.left - m.rect.width
+
+            """elif self.rect.left < m.rect.right:
+                self.rect.x = m.rect.right + m.rect.width
                 self.velocity_x = 0
             if self.rect.bottom > m.rect.top:
                 self.rect.bottom = m.rect.top
                 self.velocity_y = 0
-            if self.rect.top < m.rect.bottom:
+            elif self.rect.top < m.rect.bottom:
                 self.rect.top = m.rect.bottom
-                self.velocity_y = 0
-                print(23456)"""
-            self.velocity_x = -self.velocity_x
-            self.velocity_y = -self.velocity_y
+                self.velocity_y = 0"""
+            """self.velocity_x = -self.velocity_x
+            self.velocity_y = -self.velocity_y"""
         return len(mol_hits) > 0
 
 
     def update_parameters(self):
-        if not self.check_collisions():
-            """self.velocity_x = randint(-2, 2)
-            self.velocity_y = randint(-2, 2)"""
+        self.velocity_x = randint(-2, 2)
+        self.velocity_y = randint(-2, 2)
 
         self.rect.x += self.velocity_x
         self.rect.y += self.velocity_y
+
+        self.check_collisions()
 
         self.pos = (self.rect.x + self.radius, self.rect.y + self.radius)
 
@@ -78,21 +79,20 @@ while running:
     for e in pg.event.get():
         if e.type == pg.QUIT:
             running = False
-        if e.type == pg.KEYDOWN:
-            if e.key == pg.K_a:
-                mol1.velocity_x = -5
-            if e.key == pg.K_d:
-                mol1.velocity_x = 5
-            if e.key == pg.K_w:
-                mol1.velocity_y = -5
-            if e.key == pg.K_s:
-                mol1.velocity_y = 5
-        if e.type == pg.KEYUP:
-            if e.key == pg.K_a or e.key == pg.K_d:
-                mol1.velocity_x = 0
-            if e.key == pg.K_w or e.key == pg.K_s:
-                mol1.velocity_y = 0
 
+    key_pressed = pg.key.get_pressed()
+    if key_pressed[pg.K_a]:
+        mol1.velocity_x = -5
+    elif key_pressed[pg.K_d]:
+        mol1.velocity_x = 5
+    else:
+        mol1.velocity_x = 0
+    if key_pressed[pg.K_w]:
+        mol1.velocity_y = -5
+    elif key_pressed[pg.K_s]:
+        mol1.velocity_y = 5
+    else:
+        mol1.velocity_y = 0
 
     window.fill((255, 255, 255))
 
