82b301e3ee39cace30662993981a86714c440191
andreyshumkov <shumkovandrey520@gmail.com>
2023-10-02 15:40:53 +0500

    1.0 Added: magnetic field, brownian motion, moving the camera, ...

--- main2.py
+++ main2.py
@@ -35,11 +35,11 @@ class NonMoleculeObject(pg.sprite.Sprite):
 
     def update(self):
         self.update_parameters()
-        pg.draw.circle(window, self.color, self.pos, self.radius)
+        pg.draw.circle(window, self.color, (self.pos[0]+offset_x, self.pos[1]+offset_y), self.radius)
 
 
 class Molecule(pg.sprite.Sprite):
-    def __init__(self, pos, radius, color=(100, 100, 100), speed=2):
+    def __init__(self, pos, radius, color=(100, 100, 100), speed=2, power=100):
         super().__init__()
 
         self.pos = list(pos)
@@ -49,13 +49,14 @@ class Molecule(pg.sprite.Sprite):
         self.speed = speed
         self.velocity_x = 0
         self.velocity_y = 0
+        self.power = power
 
         objects.add_internal(self)
 
     def check_collisions(self):
         collided = []
         for i in objects.sprites():
-            if get_dist(self.pos, i.pos) < self.radius + i.radius and i != self:
+            if get_dist(self.pos, i.pos) < self.radius+0.5*self.radius + i.radius+0.5*i.radius and i != self:
                 collided.append(i)
 
         for i in collided:
@@ -67,8 +68,8 @@ class Molecule(pg.sprite.Sprite):
             ratio_x = (self.pos[0] - i.pos[0]) / dist
             ratio_y = (self.pos[1] - i.pos[1]) / dist
 
-            self.pos[0] += ratio_x * (i.radius + self.radius - dist)
-            self.pos[1] += ratio_y * (i.radius + self.radius - dist)
+            self.pos[0] += ratio_x * (i.radius+0.5*i.radius + self.radius+0.5*self.radius - dist)
+            self.pos[1] += ratio_y * (i.radius+0.5*i.radius + self.radius+0.5*self.radius - dist)
 
         if self.pos[0]-self.radius < 0:
             self.pos[0] = self.radius
@@ -84,6 +85,7 @@ class Molecule(pg.sprite.Sprite):
 
     def follow(self, lst, coe):
         if len(lst) > 0:
+            lst = lst.copy()
             if self in lst:
                 lst.remove(self)
 
@@ -91,11 +93,17 @@ class Molecule(pg.sprite.Sprite):
             for i in lst[1:]:
                 if get_dist(self.pos, i.pos) < get_dist(self.pos, near.pos):
                     near = i
-            ratio_x = (self.pos[0] - near.pos[0]) / get_dist(self.pos, near.pos)
-            ratio_y = (self.pos[1] - near.pos[1]) / get_dist(self.pos, near.pos)
 
-            self.velocity_x = self.speed * ratio_x * -coe
-            self.velocity_y = self.speed * ratio_y * -coe
+            if get_dist(self.pos, near.pos) <= self.power:
+                ratio_x = (self.pos[0] - near.pos[0]) / get_dist(self.pos, near.pos)
+                ratio_y = (self.pos[1] - near.pos[1]) / get_dist(self.pos, near.pos)
+
+                self.velocity_x += self.speed * ratio_x * -coe
+                self.velocity_y += self.speed * ratio_y * -coe
+            else:
+                self.velocity_x += self.speed * randint(-2, 2)
+                self.velocity_y += self.speed * randint(-2, 2)
+
 
     def update_parameters(self):
 
@@ -106,10 +114,13 @@ class Molecule(pg.sprite.Sprite):
         self.pos[0] += self.velocity_x
         self.pos[1] += self.velocity_y
 
+        self.velocity_x = 0
+        self.velocity_y = 0
+
 
     def update(self):
         self.update_parameters()
-        pg.draw.circle(window, self.color, self.pos, self.radius)
+        pg.draw.circle(window, self.color, (self.pos[0]+offset_x, self.pos[1]+offset_y), self.radius)
 
 
 class RedMolecule(Molecule):
@@ -118,15 +129,27 @@ class RedMolecule(Molecule):
         red_mols.append(self)
 
     def reset(self):
-        self.follow(blue_mols, -1)
+        self.follow(blue_mols, 1)
 
 class BlueMolecule(Molecule):
     def __init__(self, pos):
-        super().__init__(pos, 10, (0, 0, 255), 1)
+        super().__init__(pos, 10, (0, 0, 255), 2)
         blue_mols.append(self)
 
     def reset(self):
+        pass
+        self.follow(red_mols, -1)
+
+class YellowMolecule(Molecule):
+    def __init__(self, pos):
+        super().__init__(pos, 10, (255,255,153), 1)
+        yellow_mols.append(self)
+
+    def reset(self):
+        pass
         self.follow(red_mols, 1)
+        self.follow(blue_mols, -1)
+
 
 WIDTH = 700
 HEIGHT = 500
@@ -137,8 +160,9 @@ pg.display.set_caption('Physic simulation')
 objects = pg.sprite.Group()
 red_mols = []
 blue_mols = []
+yellow_mols = []
 
-magnit = NonMoleculeObject((0, 0), 25)
+magnit = NonMoleculeObject((0, 0), 5)
 
 for _ in range(10):
     RedMolecule((randint(0, WIDTH), randint(0, HEIGHT)))
@@ -146,8 +170,14 @@ for _ in range(10):
 for _ in range(10):
     BlueMolecule((randint(0, WIDTH), randint(0, HEIGHT)))
 
+# for _ in range(10):
+#     YellowMolecule((randint(0, WIDTH), randint(0, HEIGHT)))
+
 clock = pg.time.Clock()
-FPS = 30
+FPS = 60
+
+offset_x = 0
+offset_y = 0
 
 running = True
 
@@ -156,8 +186,17 @@ while running:
     for e in pg.event.get():
         if e.type == pg.QUIT:
             running = False
+    key_pressed = pg.key.get_pressed()
+    if key_pressed[pg.K_a]:
+        offset_x += 500 * 1/FPS
+    elif key_pressed[pg.K_d]:
+        offset_x -= 500 * 1/FPS
+    if key_pressed[pg.K_w]:
+        offset_y += 500 * 1/FPS
+    elif key_pressed[pg.K_s]:
+        offset_y -= 500 * 1/FPS
 
-    magnit.pos = list(pg.mouse.get_pos())
+    magnit.pos = [pg.mouse.get_pos()[0]-offset_x, pg.mouse.get_pos()[1]-offset_y]
 
     window.fill((255, 255, 255))
 
